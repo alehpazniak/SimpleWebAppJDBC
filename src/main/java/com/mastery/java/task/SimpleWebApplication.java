46da17d4edc689f8550346b155722eed992138e8
alehpazniak <avtovoby@gmail.com>
2021-12-18 16:59:58 +0300

    fix bugs

--- src/main/java/com/mastery/java/task/SimpleWebApplication.java
+++ src/main/java/com/mastery/java/task/SimpleWebApplication.java
@@ -1,5 +1,6 @@
 package com.mastery.java.task;
 
+import com.mastery.java.task.thread.MyThread;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleWebApplication.class, args);
+
+//        Thread myThread = new Thread(new MyThread());
+//        myThread.start();
     }
 }
